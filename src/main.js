ff4f27bbfc5ad81c8fae3f61ac6aa87f8610dd65
JoaoGabriel17 <santosjoaogabriel09@gmail.com>
2022-06-13 13:20:24 -0300

    Commit for ListenCoins

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'spectre.css/dist/spectre.min.css'
+import 'spectre.css/dist/spectre-icons.min.css'
+
+const app = createApp(App)
+app.mount('#app')
